8f49e5adf59009102a4b335fc3c6956d2351a4c4
Syed Mahbub <xsmahmed776@gmail.com>
2021-11-21 03:26:50 +0600

    ignore config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      "mongodb+srv://smahmed776:Igd13pKNJyvITayl@lifebook.p0si0.mongodb.net/nextlifebook?retryWrites=true&w=majority"
-  }
-}
+
