f242ff6e2271cc500d9f9fe7d208b914edcd2beb
Syed Mahbub <xsmahmed776@gmail.com>
2021-11-26 23:08:40 +0600

    api change

--- components/API/API.js
+++ components/API/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "http://localhost:5000/api/auth/v1/",
+    baseURL: "https://lifebooksocial.herokuapp.com/api/auth/v1/",
     withCredentials: true,
     credentials: "include"
 })
