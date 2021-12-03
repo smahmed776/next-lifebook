ead6368b17ef9828d9579362fb5eb0632ed5c22c
Syed Mahbub <xsmahmed776@gmail.com>
2021-12-03 22:36:57 +0600

    api env

--- components/API/API.js
+++ components/API/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: process.env.API_URI,
+    baseURL: process.env.NODE_ENV === "production" ? process.env.API_URI : "http://localhost:5000/api/auth/v1/",
     withCredentials: true,
     credentials: "include"
 })
--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
   env: {
     MONGO_URI:
       "mongodb+srv://smahmed776:Igd13pKNJyvITayl@lifebook.p0si0.mongodb.net/nextlifebook?retryWrites=true&w=majority",
-      API_URI: "http://localhost:5000/api/auth/v1/"
+      API_URI: "https://lifebooksocial.herokuapp.com/api/auth/v1/"
   }
 }
